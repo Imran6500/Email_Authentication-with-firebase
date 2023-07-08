c50fc6d93592049c61731843479631c0f442b7da
Imran6500 <ih8237728@gamil.com>
2023-07-08 20:21:11 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterapp/auth/login.dart';
+import 'package:flutterapp/pages/home.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Home();
+            } else {
+              return LoginForm();
+            }
+          },
+        ));
+  }
+}
